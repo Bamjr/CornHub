aa7f513e979fcfa23f9a41ec1b83e94f0ff84cbd
SUTTHIDA JOIROONG <sutthida.joi04@gmail.com>
2025-04-28 19:49:09 +0700

    please work before i do the suicide

--- app/landmark/[id]/page.tsx
+++ app/landmark/[id]/page.tsx
@@ -1,47 +1,45 @@
-import { fetchLandmarkDetail } from "@/actions/actions"
-import FavoriteToggleButton from "@/components/card/FavoriteToggleButton"
-import Breadcrumbs from "@/components/landmark/Breadcrumb"
-import Description from "@/components/landmark/description"
-import ImageContainer from "@/components/landmark/ImageContainer"
-import ShareButton from "@/components/landmark/ShareButton"
-import MapLandmark from "@/components/map/MapLandmark"
-import { redirect } from "next/navigation"
+import { fetchLandmarkDetail } from "@/actions/actions";
+import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
+import Breadcrumbs from "@/components/landmark/Breadcrumb";
+import Description from "@/components/landmark/description";
+import ImageContainer from "@/components/landmark/ImageContainer";
+import ShareButton from "@/components/landmark/ShareButton";
+import MapLandmark from "@/components/map/MapLandmark";
+import { redirect } from "next/navigation";
 
+type Params = { id: string };
 
-type PageProps = {
-    params: { id: string }
-  };
-  
-  const LandmarkDetail = async ({ params }: PageProps) => {
-    const { id } = params;
-  
-    const landmark = await fetchLandmarkDetail({ id });
-  
-    if (!landmark) {
-      redirect('/');
-    }
-  
-    return (
+const LandmarkDetail = async ({ params }: { params: Params }) => {
+  const { id } = params;
+
+  const landmark = await fetchLandmarkDetail({ id });
+
+  if (!landmark) {
+    redirect('/');
+  }
+
+  return (
+    <section>
+      <Breadcrumbs name={landmark.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold">{landmark.name}</h1>
+        <div className="flex items-center p-5 gap-x-4">
+          <ShareButton landmarkId={landmark.id} name={landmark.name} />
+          <FavoriteToggleButton landmarkId={landmark.id} />
+        </div>
+      </header>
+
       <section>
-        <Breadcrumbs name={landmark.name} />
-        <header className="flex justify-between items-center mt-4">
-          <h1 className="text-4xl font-bold">{landmark.name}</h1>
-          <div className="flex items-center p-5 gap-x-4">
-            <ShareButton landmarkId={landmark.id} name={landmark.name} />
-            <FavoriteToggleButton landmarkId={landmark.id} />
-          </div>
-        </header>
-  
-        <section>
-          <ImageContainer mainImage={landmark.image} name={landmark.name} />
-          <Description description={landmark.description} />
-          {landmark.lat !== 0 && landmark.lng !== 0 && (
-            <MapLandmark location={{ lat: landmark.lat, lng: landmark.lng }} />
-          )}
-        </section>
+        <ImageContainer mainImage={landmark.image} name={landmark.name} />
+        <Description description={landmark.description} />
+        {landmark.lat !== 0 && landmark.lng !== 0 && (
+          <MapLandmark location={{ lat: landmark.lat, lng: landmark.lng }} />
+        )}
       </section>
-    );
-  };
-  
-  export default LandmarkDetail;
+    </section>
+  );
+};
+
+export default LandmarkDetail;
+
   
