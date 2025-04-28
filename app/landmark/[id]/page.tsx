c880c8c0f306fd337cd3e6e587664f6d014424ff
SUTTHIDA JOIROONG <sutthida.joi04@gmail.com>
2025-04-28 19:11:23 +0700

    fix landmark

--- app/landmark/[id]/page.tsx
+++ app/landmark/[id]/page.tsx
@@ -7,33 +7,38 @@ import ShareButton from "@/components/landmark/ShareButton"
 import MapLandmark from "@/components/map/MapLandmark"
 import { redirect } from "next/navigation"
 
-
-
-const LandmarkDetail = async ({ params }: { params: { id: string } }) => {
-    const { id } = await params
-    const landmark = await fetchLandmarkDetail({ id })
-    if (!landmark) redirect('/')
-    return (
-        <section>
-            <Breadcrumbs name={landmark.name} />
-            <header className="flex justify-between items-center mt-4">
-                <h1 className="text-4xl font-bold">{landmark.name}</h1>
-                <div className="flex items-center p-5 gap-x-4">
-                    <ShareButton landmarkId={landmark.id} name={landmark.name}/>
-                    <FavoriteToggleButton landmarkId={landmark.id} />
-                </div>
-            </header>
-
-            <section>
-                <ImageContainer mainImage={landmark.image} name={landmark.name} />
-
-                <Description description={landmark.description} />
-
-                {landmark.lat !== 0 && landmark.lng !== 0 && (
-                    <MapLandmark location={{ lat: landmark.lat, lng: landmark.lng }} />
-                )}
-            </section>
-        </section>
-    )
+interface LandmarkDetailPageProps {
+  params: {
+    id: string
+  }
 }
+
+const LandmarkDetail = async ({ params }: LandmarkDetailPageProps) => {
+  const { id } = params
+  const landmark = await fetchLandmarkDetail({ id })
+
+  if (!landmark) redirect('/')
+
+  return (
+    <section>
+      <Breadcrumbs name={landmark.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold">{landmark.name}</h1>
+        <div className="flex items-center p-5 gap-x-4">
+          <ShareButton landmarkId={landmark.id} name={landmark.name} />
+          <FavoriteToggleButton landmarkId={landmark.id} />
+        </div>
+      </header>
+
+      <section>
+        <ImageContainer mainImage={landmark.image} name={landmark.name} />
+        <Description description={landmark.description} />
+        {landmark.lat !== 0 && landmark.lng !== 0 && (
+          <MapLandmark location={{ lat: landmark.lat, lng: landmark.lng }} />
+        )}
+      </section>
+    </section>
+  )
+}
+
 export default LandmarkDetail
