06d868b7fa76892821f2c24f04143fffbff40406
SUTTHIDA JOIROONG <sutthida.joi04@gmail.com>
2025-04-28 20:17:28 +0700

    fix page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
-import LoadingCard from "@/components/card/LoadingCard"
-import CornContainer from "@/components/home/CornContainer"
-import { Suspense } from "react"
+import LoadingCard from "@/components/card/LoadingCard";
+import CornContainer from "@/components/home/CornContainer";
+import { Suspense } from "react";
 
-const page = async({ searchParams }: { searchParams: { search?: string,category?:string } }) => {
-  const {search,category} = await searchParams;
+type PageProps = {
+  searchParams: Promise<{ search?: string; category?: string }>;
+};
+
+export default async function Page({ searchParams }: PageProps) {
+  const { search, category } = await searchParams;
+
   return (
-    <>
-      <Suspense fallback={<LoadingCard />}>
-        <CornContainer search={search} category={category}/>
-
-      </Suspense>
-
-    </>
-  )
-
-
+    <Suspense fallback={<LoadingCard />}>
+      <CornContainer search={search} category={category} />
+    </Suspense>
+  );
 }
-export default page
